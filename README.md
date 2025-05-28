1c7a69954c2316d95152008d3fff85af17064d22
Adil-Mohamed7 <adilmohamedparayil@gmail.com>
2025-05-29 00:23:35 +0530

    feat: add details

--- README.md
+++ README.md
@@ -64,8 +64,8 @@ Make sure you have the following installed on your machine:
 **Cloning the Repository**
 
 ```bash
-git clone https://github.com/adrianhajdin/portfolio.git
-cd portfolio
+git clone git@github.com:Adil-Mohamed7/portfolio-adil.git
+cd portfolio-adil
 ```
 
 **Installation**
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["ReactJS", "Express", "Typescript"];
-  const rightLists = ["VueJS", "NuxtJS", "GraphQL"];
+  const leftLists = ["ReactJS", "Flutter", "Typescript"];
+  const rightLists = ["NextJS", "Javascript", "Dart"];
 
   const [copied, setCopied] = useState(false);
 
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "adilmohamedparayil@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
